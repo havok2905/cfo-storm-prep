23f269623f48062c79b977e13630939579887154
Chris McLean <havok2905@gmail.com>
2017-09-30 17:34:04 -0400

    ui updates

--- src/app/survival-kit/survival-kit.component.ts
+++ src/app/survival-kit/survival-kit.component.ts
@@ -14,7 +14,8 @@ export class SurvivalKitComponent implements OnInit {
   private seeds: Array<SurvivalKitItem> = [
     new SurvivalKitItem(1, 'Pocket Radio', false, 'https://www.amazon.com/Sony-ICFP26-Portable-Radio-Black/dp/B012F0DGYE'),
     new SurvivalKitItem(2, '3 Gallons of Water Per Person', false, 'https://www.amazon.com/Gallon-Plastic-Water-Bottle-Handle/dp/B003VSEGCQ'),
-    new SurvivalKitItem(3, 'First Aid Kit', false, 'https://www.amazon.com/Coleman-Expedition-205-Piece-Emergencies-Workplace/dp/B00GOPNO6C')
+    new SurvivalKitItem(3, 'First Aid Kit', false, 'https://www.amazon.com/Coleman-Expedition-205-Piece-Emergencies-Workplace/dp/B00GOPNO6C'),
+    new SurvivalKitItem(4, '12 Gallons Of Beer Per Person', false, '#')
   ];
 
   public progress: number = 0;
