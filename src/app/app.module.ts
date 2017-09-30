d537030b2db5ebfc7c7fff15d9c3fd1a9d576d2a
Chris McLean <havok2905@gmail.com>
2017-09-30 09:03:31 -0400

    added md-card support

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MdToolbarModule } from '@angular/material';
+import { MdToolbarModule, MdCardModule } from '@angular/material';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
@@ -18,7 +18,8 @@ import { ServicesDirectoryComponent } from './services-directory/services-direct
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MdToolbarModule
+    MdToolbarModule,
+    MdCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
