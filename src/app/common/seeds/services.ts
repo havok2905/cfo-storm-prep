0a3fea41af691c4e20a54665a6eaf8d4f32d182e
Chris McLean <havok2905@gmail.com>
2017-10-03 00:48:29 -0400

    abstracted out services instances into a seeds file

---
+++ src/app/common/seeds/services.ts
@@ -0,0 +1,6 @@
+import { ServiceItem } from '../entities/service-item';
+
+export default [
+  new ServiceItem('Orlando’s Emergency Radio Station 1650AM', 'N/A', 'N/A', 'http://www.cityoforlando.net/emergency/'),
+  new ServiceItem('Orlando OEM', '321.235.5438', 'N/A', 'http://www.cityoforlando.net/emergency/contacts/')
+];
--- src/app/services-directory/services-directory-data-source.ts
+++ src/app/services-directory/services-directory-data-source.ts
@@ -1,3 +1,4 @@
+import SERVICE_ITEMS from '../common/seeds/services';
 import { ServiceItem } from '../common/entities/service-item';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/of';
@@ -5,10 +6,7 @@ import { DataSource } from '@angular/cdk/collections';
 
 export class ServicesDirectoryDataSource extends DataSource<any> {
   connect(): Observable<ServiceItem[]> {
-    return Observable.of([
-      new ServiceItem('Orlando’s Emergency Radio Station 1650AM', 'N/A', 'N/A', 'http://www.cityoforlando.net/emergency/'),
-      new ServiceItem('Orlando OEM', '321.235.5438', 'N/A', 'http://www.cityoforlando.net/emergency/contacts/')
-    ]);
+    return Observable.of(SERVICE_ITEMS);
   }
 
   disconnect(): void {}
