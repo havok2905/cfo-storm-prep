a91f3cf4e53a0b7763d97c4323a1ed764522e2a4
Chris McLean <havok2905@gmail.com>
2017-10-03 01:38:39 -0400

    added more survival kit seeds

--- src/app/common/seeds/survival-kit-items.ts
+++ src/app/common/seeds/survival-kit-items.ts
@@ -1,8 +1,17 @@
 import SurvivalKitItem from '../entities/survival-kit-item';
 
 export default [
-  new SurvivalKitItem(1, 'Pocket Radio', false, 'https://www.amazon.com/Sony-ICFP26-Portable-Radio-Black/dp/B012F0DGYE'),
-  new SurvivalKitItem(2, '3 Gallons of Water Per Person', false, 'https://www.amazon.com/Gallon-Plastic-Water-Bottle-Handle/dp/B003VSEGCQ'),
+  new SurvivalKitItem(1, 'Non Perishable Food For 3 To 7 days', false, null),
+  new SurvivalKitItem(2, '1 Gallon Daily Per Person', false, 'https://www.amazon.com/Gallon-Plastic-Water-Bottle-Handle/dp/B003VSEGCQ'),
   new SurvivalKitItem(3, 'First Aid Kit', false, 'https://www.amazon.com/Coleman-Expedition-205-Piece-Emergencies-Workplace/dp/B00GOPNO6C'),
-  new SurvivalKitItem(4, '12 Gallons Of Beer Per Person', false, '#')
+  new SurvivalKitItem(4, 'Pocket Radio', false, 'https://www.amazon.com/Sony-ICFP26-Portable-Radio-Black/dp/B012F0DGYE'),
+  new SurvivalKitItem(5, 'Non Electric Can Opener', false, 'https://www.amazon.com/OXO-Good-Grips-Can-Opener/dp/B00004OCJW'),
+  new SurvivalKitItem(6, 'Cooking Tools', false, null),
+  new SurvivalKitItem(7, 'Fuel', false, null),
+  new SurvivalKitItem(8, 'Sturdy Shoes', false, null),
+  new SurvivalKitItem(9, 'Toiletries', false, null),
+  new SurvivalKitItem(10, 'Flashlight', false, 'https://www.amazon.com/Rockbirds-Flashlights-Flashlight-Resistant-Tactical/dp/B00GXX2CHM'),
+  new SurvivalKitItem(11, 'Batteries', false, null),
+  new SurvivalKitItem(12, 'Cash', false, null),
+  new SurvivalKitItem(13, 'Various Tools', false, 'https://www.amazon.com/Stanley-94-248-65-Piece-Homeowners-Tool/dp/B000UHMITE'),
 ];
