6d730cd06e5e8e2534be986819d9e02a4d7a4b84
Chris McLean <havok2905@gmail.com>
2017-10-05 00:10:19 -0400

    added more real home prep options

--- src/app/common/seeds/home-prep-items.ts
+++ src/app/common/seeds/home-prep-items.ts
@@ -1,7 +1,15 @@
 import HomePrepItem from '../entities/home-prep-item';
 
 export default [
-  new HomePrepItem(1, 'Board Up All Windows', false),
-  new HomePrepItem(2, 'Locate Your Nearest Shelter', false),
-  new HomePrepItem(3, 'Fill Up Your Gas Tank', false)
+  new HomePrepItem(1, 'Collect Important IDs and Documentation for Evacutation', false),
+  new HomePrepItem(2, 'Board Up And Close All Windows', false),
+  new HomePrepItem(3, 'Bring All Loose Items And Furniture Inside', false),
+  new HomePrepItem(4, 'Locate Your Nearest Shelter', false),
+  new HomePrepItem(5, 'Fill Up Your Gas Tank', false),
+  new HomePrepItem(6, 'Turn Your Freezer To The Highest Setting', false),
+  new HomePrepItem(7, 'Turn Off All Propane Tanks', false),
+  new HomePrepItem(8, 'Unplug Small Appliances', false),
+  new HomePrepItem(9, 'Create An Evacuation Plan', false),
+  new HomePrepItem(10, 'Charge All Electronic Devices and Extra Batteries', false),
+  new HomePrepItem(11, 'Prepare A Saferoom Away From Any Windows', false)
 ];
